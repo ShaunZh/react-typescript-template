383fd92788cf15e712b9600453d3a2ab79410916
ShaunZh <308826842@qq.com>
2019-10-28 10:24:41 +0800

    add: 添加webpack和typescript的文件夹别名支持

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,21 @@
 // eslint-disable-next-line
-const { override, fixBabelImports } = require('customize-cra')
+const { override, fixBabelImports, addWebpackAlias} = require('customize-cra')
+// eslint-disable-next-line
+const path = require('path')
+
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd-mobile',
     libraryDirectory: 'es',
     style: 'css'
-  })
+  }),
+  // 重写webpack的alias（别名）
+  addWebpackAlias({
+    '@': path.resolve(__dirname, 'src'),
+    '@components': path.resolve(__dirname, 'src/components'),
+    '@containers': path.resolve(__dirname, 'src/containers'),
+    '@api': path.resolve(__dirname, 'src/api'),
+    '@utils': path.resolve(__dirname, 'src/utils')
+  }),
 )
