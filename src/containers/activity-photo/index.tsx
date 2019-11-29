dcc4afef448d3b5d12e519bb05debbc793e4a5a1
ShaunZh <308826842@qq.com>
2019-11-29 16:06:32 +0800

    add: 测试引入scss文件

--- src/containers/activity-photo/index.tsx
+++ src/containers/activity-photo/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import userApi from '@api/user'
 import { NavLink } from 'react-router-dom'
+import './index.scss'
 
 // url地址信息
 interface Props {
@@ -19,7 +20,7 @@ class ActivityPhoto extends React.Component<Props, object> {
   public render() {
     return (
       <div className="activity-photo-container">
-        <h1 style={{ fontSize: '24px' }}>活动照片</h1>
+        <h1>活动照片</h1>
         <NavLink to="/test">go to test</NavLink>
       </div>
     )
