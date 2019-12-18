256bc741862ccaf83e93949400be5e897cc39608
ShaunZh <308826842@qq.com>
2019-12-18 17:15:06 +0800

    update: 路由权限

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -7,33 +7,27 @@ import NoAuth from '@/containers/noAuth'
 const routes = [
   {
     path: '/activity-photo',
-    auth: ['user'],
-    exact: true,
     component: ActivityPhoto
   },
   {
     path: '/today-status',
     auth: ['user'],
-    exact: true,
     component: TodayStatus
   },
   {
     path: '/daily-food',
-    auth: ['admin'],
-    exact: true,
+    auth: ['user'],
     redirectPath: '/no-auth',
     component: DailyFood
   },
   {
     path: '/more',
     auth: ['user'],
-    exact: true,
     component: More
   },
   // 无权限页面
   {
     path: '/no-auth',
-    exact: true,
     component: NoAuth
   }
 ]
