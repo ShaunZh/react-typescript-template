6854c1d7f84647f69f88703211e2b609c0947d5d
ShaunZh <308826842@qq.com>
2019-12-18 14:33:41 +0800

    update: 将布局组件与授权结合

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -3,17 +3,27 @@
  * @Author: Hexon
  * @Date: 2019-10-28 17:26:29
  * @LastEditors: Hexon
- * @LastEditTime: 2019-10-29 16:29:12
+ * @LastEditTime: 2019-12-18 11:41:03
  */
 
 import * as React from 'react'
 import { TabBar } from 'antd-mobile'
 import { createHashHistory } from 'history'
+import { connect, ConnectedProps } from 'react-redux'
 import statusImg from '../assets/images/status.svg'
 import statusActiveImg from '../assets/images/status-active.svg'
-
 import payImg from '../assets/images/pay.svg'
 import payActiveImg from '../assets/images/pay-active.svg'
+
+import authApi from '@/api/auth'
+import locationSearch from '@/utils/locationSearch'
+import WxAuth from '@/utils/wxAuth'
+import { UserState, USER_UPDATE } from '@/redux/modules/user/types'
+import { AuthState, AUTH_UPDATE } from '@/redux/modules/auth/types'
+import { AppState } from '@/redux/'
+import { setToken } from '@/utils/auth'
+
+const WxInstance = WxAuth.getInstance()
 
 export enum eMenu {
   activityPhoto = 'activityPhoto',
@@ -22,34 +32,109 @@ export enum eMenu {
   more = 'more',
   empty = ''
 }
+
+const mapState = (state: AppState) => ({
+  user: state.user,
+  token: state.auth.token
+})
+
+const mapDispatch = {
+  updateToken: (token: AuthState) => ({ type: AUTH_UPDATE, payload: token }),
+  updateUser: (userInfo: UserState) => ({ type: USER_UPDATE, payload: userInfo })
+}
+
+const connector = connect(
+  mapState,
+  mapDispatch
+)
+
 interface State {
   hidden: boolean
   currentTab: eMenu
 }
-interface Props {
-  children: React.ReactChild // 子组件
-  // isFooterVisible?: boolean // 是否显示
-  // location?: Location
-  currentTab?: eMenu
+interface PropsParent {
+  children: React.ReactNode // 子组件
 }
 
-export default class MainLayout extends React.Component<Props, State> {
+interface HttpResponseAuth extends HttpResponse {
+  result: {
+    Authorization: string
+  }
+}
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+type Props = PropsParent & PropsFromRedux
+
+class MainLayout extends React.Component<Props, State> {
   public constructor(props: Props) {
     super(props)
     this.state = {
-      currentTab: this.props.currentTab || eMenu.activityPhoto,
+      currentTab: eMenu.activityPhoto,
       hidden: false
     }
   }
 
-  public componentDidMount() {
-    console.log('main-layout componentDidMount')
+  public async componentDidMount() {
+    // 根据window.location.hash来设置state.currentTab以便初始化激活的底部talocation.hash来设置state.currentTab以便初始化激活的底部tabb
+    console.log('props', window.location.hash)
+    const searchParams = locationSearch(window.location.search)
+    try {
+      const resp = await WxInstance.wxAuth(authApi.authWxJssdk, authApi.auth, {
+        auth: {
+          code: searchParams.code,
+          type: 'parent',
+          clientKey: searchParams.clientKey || ''
+        },
+        signature: {
+          clientKey: searchParams.clientKey || '',
+          url: window.location.href.split('#')[0]
+        }
+      })
+      let authInfo: HttpResponseAuth = WxInstance.getAuthFromSession()
+      switch (resp.authStatus) {
+        // case 'authSuccess':
+        //   setToken(resp.authInfo.result.Authorization)
+        //   console.log('authSuccess')
+        //   break
+        case 'authed':
+          if (authInfo && authInfo.result && authInfo.result.Authorization) {
+            setToken(authInfo.result.Authorization)
+            // 已经授权过的，无操作
+            this.props.updateToken({
+              token: authInfo.result.Authorization
+            })
+
+            this.props.updateUser({
+              ...this.props.user,
+              role: 'user'
+            })
+            console.log('authed')
+          }
+          break
+        case 'noAuth':
+          this.props.updateToken({
+            token: 'ttttt'
+          })
+          this.props.updateUser({
+            ...this.props.user,
+            role: 'user'
+          })
+          console.log('noAuth')
+          break
+        default:
+          break
+      }
+    } catch (e) {
+      // 授权失败的跳转到白板页面
+      console.log('err: ', e.message)
+    }
   }
 
   public render() {
     return (
       <div className="layout">
-        <div className="main-container">{this.props.children}</div>
+        <div className="main-container">{this.props.token.length ? this.props.children : '暂无授权'}</div>
         <div className="nav-tab-container" style={{ position: 'fixed', height: '50px', width: '100%', bottom: 0 }}>
           <TabBar
             tabBarPosition="bottom"
@@ -180,3 +265,5 @@ export default class MainLayout extends React.Component<Props, State> {
     )
   }
 }
+
+export default connector(MainLayout)
