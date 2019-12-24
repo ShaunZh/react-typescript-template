290afaea736a0c7299001e63748d14a62c196554
ShaunZh <308826842@qq.com>
2019-12-24 16:55:45 +0800

    remove: api请求头，请求头在环境变量中设置

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,11 +2,11 @@ import axios from '@/plugins/axios/index'
 
 // 微信授权
 function auth<T>(params: T) {
-  return axios.post('/coman/auth/authCodeOfPat', params)
+  return axios.post('/auth/authCodeOfPat', params)
 }
 
 function authWxJssdk<T>(params: T): Promise<any> {
-  return axios.post('/coman/weixin/getJsapiSignature', params)
+  return axios.post('/weixin/getJsapiSignature', params)
 }
 
 export default {
